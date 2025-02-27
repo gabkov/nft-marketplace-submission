71817bb7c9c2a734100f5f5f8ce1d28485ec8e93
Gabor Kovacs <gabor.kovacs@dakai.io>
2025-02-27 17:48:02 +0100

    test: add cancelListing test and gas snapshot

---
+++ test/unit/concrete/cancelListing/cancelListing.t.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "test/NftMarketplace.t.sol";
+
+contract CancelListingTest is NftMarketplaceTest {
+    function setUp() public override {
+        super.setUp();
+    }
+
+    function test_WhenCallerIsNotOwner() external {
+        // It should revert with {NotTokenOwner}
+
+        vm.prank({msgSender: bob});
+        vm.expectRevert(INftMarketPlace.NotTokenOwner.selector);
+        nftm.cancelListing(mockNft, token1);
+    }
+
+    modifier whenCallerIsTheOwner() {
+        vm.startPrank({msgSender: alice});
+        _;
+    }
+
+    function test_WhenItemIsNotListed() external whenCallerIsTheOwner {
+        // It should revert with {ListingNotFound}
+
+        vm.expectRevert(abi.encodeWithSelector(INftMarketPlace.ListingNotFound.selector, mockNft, 0));
+        nftm.cancelListing(mockNft, token1);
+    }
+
+    function test_WhenItemIsListed() external whenCallerIsTheOwner {
+        // It should delete the listing
+        // It should emit {ListingCancelled} event
+
+        MockNft(mockNft).approve(address(nftm), token1);
+        nftm.listItem(mockNft, token1, 1 ether);
+
+        // verify that it was listed
+        (address seller, uint256 price) = getSellerAndPrice(mockNft, token1);
+        assertEq(seller, alice);
+        assertEq(price, 1 ether);
+
+        vm.expectEmit();
+        emit INftMarketPlace.ListingCancelled(mockNft, token1);
+        nftm.cancelListing(mockNft, token1);
+
+        (seller, price) = getSellerAndPrice(mockNft, token1);
+        assertEq(seller, address(0));
+        assertEq(price, 0);
+    }
+
+    function testGas_cancelListing() external whenCallerIsTheOwner {
+        MockNft(mockNft).approve(address(nftm), token1);
+        nftm.listItem(mockNft, token1, 1 ether);
+
+        nftm.cancelListing(mockNft, token1);
+        vm.snapshotGasLastCall("cancelListing");
+    }
+}
